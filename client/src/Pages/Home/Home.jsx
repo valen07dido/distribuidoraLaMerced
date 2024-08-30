3aa780dea7c9678eaca21876cca383c715f54650
valen07dido <valendido69@gmail.com>
2024-08-30 13:51:33 -0300

    cambios en el carrousel

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -6,6 +6,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // Importa los e
 import prueba from "../../../public/productos/Criadores/Criadores_Frente_18x18_RGB_300dpi_Gris.jpg";
 import banner1 from "../../../public/imagenes/banner1.jpg";
 import banner2 from "../../../public/imagenes/banner2.jpg";
+import banner3 from "../../../public/imagenes/banner3.jpg";
+import banner4 from "../../../public/imagenes/banner4.jpg";
 import { Link } from "react-router-dom";
 import { IoIosArrowDown } from "react-icons/io";
 
@@ -14,25 +16,33 @@ const Home = () => {
     <div className={styles.container}>
       <Carousel
         autoPlay
-        infiniteLoop
+        infiniteLoop={true}
         showThumbs={false}
+        swipeable={true}
         showStatus={false}
         showArrows={false}
         showIndicators={false}
         swipeScrollTolerance={5}
-        interval={3000} // Tiempo de cambio de cada imagen (3 segundos)
+        interval={3000} 
         className={styles.carousel}
       >
         <div>
-          <img src={banner1} alt="Banner 1" />
+          <img src={banner1} alt="Banner 1"  className={styles.carouselImg}/>
         </div>
         <div>
-          <img src={banner2} alt="Banner 2" />
+          <img src={banner2} alt="Banner 2" className={styles.carouselImg}/>
+        </div>
+        <div>
+          <img src={banner3} alt="Banner 3" className={styles.carouselImg}/>
+        </div>
+        <div>
+          <img src={banner4} alt="Banner 4" className={styles.carouselImg}/>
         </div>
       </Carousel>
       <div className={styles.products}>
         <h1>Nuestros productos</h1>
         <div className={styles.grid}>
+          <Card image={prueba} title="Criadores" />
           <Card image={prueba} title="Criadores" />
           <Card image={prueba} title="Criadores" />
           <Card image={prueba} title="Criadores" />
