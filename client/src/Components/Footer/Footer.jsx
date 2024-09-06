419e91ad34c3be548f90c3df1eb2c9971e493446
valen07dido <valendido69@gmail.com>
2024-09-06 14:10:37 -0300

    desarrollo del footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -11,9 +11,15 @@ const Footer = () => {
       <div className={styles.containerInfo}>
         <img src={logo} alt="logo" className={styles.logo} />
         <div className={styles.LinkContainer}>
-          <Link to="/" className={styles.text}>Home</Link>
-          <Link to="/nosotros" className={styles.text}>About</Link>
-          <Link to="/contacto" className={styles.text}>Contact</Link>
+          <Link to="/" className={styles.text}>
+            Home
+          </Link>
+          <Link to="/nosotros" className={styles.text}>
+            About
+          </Link>
+          <Link to="/contacto" className={styles.text}>
+            Contact
+          </Link>
         </div>
         <div className={styles.LinkContainer}>
           <Link to="" className={styles.text}>
@@ -25,9 +31,6 @@ const Footer = () => {
           <Link to="" className={styles.text}>
             <FaFacebook /> Facebook
           </Link>
-          <Link to="" className={styles.text}>
-            <SiGmail /> E-mail
-          </Link>
         </div>
         <div className={styles.containerMap}>
           <p className={styles.location}>
@@ -35,12 +38,16 @@ const Footer = () => {
             Blvd. Colón 2601, S2170 Casilda, Santa Fe
           </p>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d208.99907304778876!2d-61.163776931060355!3d-33.05685729493616!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7d3f1b754a959%3A0x8e50043216d83a40!2sDistribuidora%20%22La%20Merced%E2%80%9D%20-%20Alimento%20Balanceado!5e0!3m2!1ses!2sar!4v1725585548740!5m2!1ses!2sar"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13375.94403208508!2d-61.1637438!3d-33.0568352!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7d3f1b754a959%3A0x8e50043216d83a40!2sDistribuidora%20%22La%20Merced%E2%80%9D%20-%20Alimento%20Balanceado!5e0!3m2!1ses!2sar!4v1725640863637!5m2!1ses!2sar"
             allowfullscreen=""
             loading="lazy"
+            className={styles.map}
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
+      </div>
+      <div>
+        <p className={styles.copyright}>©Todos los derechos reservados 2024</p>
       </div>
     </div>
   );
