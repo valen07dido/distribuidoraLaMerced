21534e1cb647c5ddb4438dbc2c21a1e516217200
valen07dido <valendido69@gmail.com>
2024-09-09 18:16:46 -0300

    algo del about

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import styles from "./About.module.css"
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div className={styles.globalContainer}>
+      <div className={styles.container}>
+        <h1>Un poco de nuestra historia</h1>
+      </div>
+    </div>
+  );
 };
 
 export default About;
