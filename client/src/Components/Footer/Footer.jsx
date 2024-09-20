c47d8e451b72d3e8a534834ec63fbe8a1b012589
valen07dido <valendido69@gmail.com>
2024-09-20 00:49:01 -0300

    about

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -32,19 +32,6 @@ const Footer = () => {
             <FaFacebook /> Facebook
           </Link>
         </div>
-        <div className={styles.containerMap}>
-          <p className={styles.location}>
-            <FaLocationDot />
-            Blvd. Colón 2601, S2170 Casilda, Santa Fe
-          </p>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13375.94403208508!2d-61.1637438!3d-33.0568352!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7d3f1b754a959%3A0x8e50043216d83a40!2sDistribuidora%20%22La%20Merced%E2%80%9D%20-%20Alimento%20Balanceado!5e0!3m2!1ses!2sar!4v1725640863637!5m2!1ses!2sar"
-            allowFullScreen
-            referrerPolicy="no-referrer"
-            loading="lazy"
-            className={styles.map}
-          ></iframe>
-        </div>
       </div>
       <div>
         <p className={styles.copyright}>©Todos los derechos reservados 2024</p>
--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./About.module.css";
+import { FaLocationDot } from "react-icons/fa6";
+
 const About = () => {
   return (
     <div className={styles.globalContainer}>
       <div className={styles.container}>
-        <h1>La Merced, nuestra Empresa Familiar</h1>
+        <h1 className={styles.title}>La Merced, nuestra Empresa Familiar</h1>
         <h3
           className={styles.text}
           data-aos="fade-right"
@@ -20,7 +22,7 @@ const About = () => {
         </h3>
         <h3
           className={styles.text}
-          data-aos="fade-left"
+          data-aos="fade-right"
           data-aos-duration="1000"
         >
           La Merced cumple la función de distribuidora en productos destinados a
@@ -39,7 +41,7 @@ const About = () => {
 
         <h3
           className={styles.text}
-          data-aos="fade-left"
+          data-aos="fade-right"
           data-aos-duration="1000"
         >
           Juntos, seguimos avanzando con la convicción de que una empresa sólida
@@ -47,8 +49,30 @@ const About = () => {
           amor por lo que hacemos. En La Merced, cada día es una nueva
           oportunidad para crecer y servir a quienes confían en nosotros.
         </h3>
+        <h2 className={styles.subtitle}>Nuesta ubicacion</h2>
+        <div className={styles.containerMap}>
+          <h3 className={styles.text}>
+            Queremos conocerte y compartir nuestra pasión por el bienestar de
+            tus mascotas. Ven a visitarnos en Calle{" "}
+            <b>
+              <FaLocationDot />
+              Blvd. Colón 2601, en la localidad de Casilda, Santa Fe
+            </b>
+            . Donde podrás explorar nuestra amplia gama de productos diseñados
+            especialmente para ellos. Nuestro equipo estará encantado de
+            asesorarte y ayudarte a encontrar lo que mejor se adapte a tus
+            necesidades.
+          </h3>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13375.94403208508!2d-61.1637438!3d-33.0568352!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7d3f1b754a959%3A0x8e50043216d83a40!2sDistribuidora%20%22La%20Merced%E2%80%9D%20-%20Alimento%20Balanceado!5e0!3m2!1ses!2sar!4v1725640863637!5m2!1ses!2sar"
+            allowFullScreen
+            referrerPolicy="no-referrer"
+            loading="lazy"
+            className={styles.map}
+          ></iframe>
+        </div>
         {/* <img src="../../../public/imagenes/Family.png" alt="family" /> */}
-        <h2>Organización</h2>
+        <h2 className={styles.subtitle}>Organización</h2>
         <h3 className={styles.text}>
           <b>Socio gerente:</b>Joaquín Marini
         </h3>
