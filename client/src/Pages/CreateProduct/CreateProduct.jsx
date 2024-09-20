c839d209e1ba6c4b2e4b4dd7d4505a4a0549d764
valen07dido <valendido69@gmail.com>
2024-09-19 23:33:23 -0300

    form de creacion de producto

--- client/src/Pages/CreateProduct/CreateProduct.jsx
+++ client/src/Pages/CreateProduct/CreateProduct.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./CreateProduct.module.css";
+
 const CreateProduct = () => {
   const [product, setProduct] = useState({
     name: "",
@@ -24,6 +25,7 @@ const CreateProduct = () => {
     types: [],
   });
 
+  console.log(product);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setProduct((prevProduct) => ({
@@ -76,6 +78,23 @@ const CreateProduct = () => {
     }));
   };
 
+  const addFeedingGuide = () => {
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      feedingGuide: [
+        ...prevProduct.feedingGuide,
+        { peso_min: "", peso_max: "", racion_min: "", racion_max: "" },
+      ],
+    }));
+  };
+
+  const removeFeedingGuide = (index) => {
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      feedingGuide: prevProduct.feedingGuide.filter((_, i) => i !== index),
+    }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // todo: Aquí agregar la lógica para enviar los datos a la API
@@ -85,10 +104,10 @@ const CreateProduct = () => {
   return (
     <div className={styles.globalContainer}>
       <div className={styles.container}>
-        <h1 className={styles.title}>Create Product</h1>
+        <h1 className={styles.title}>Crea un nuevo producto</h1>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="name">Product Name:</label>
+            <label htmlFor="name">Nombre:</label>
             <input
               type="text"
               id="name"
@@ -98,49 +117,34 @@ const CreateProduct = () => {
               required
             />
           </div>
-          <div>
-            <label htmlFor="description">Description:</label>
+          <div className={styles.containTextarea}>
+            <label htmlFor="description">Descripcion:</label>
             <textarea
               id="description"
               name="description"
               value={product.description}
               onChange={handleChange}
+              className={styles.textarea}
               required
             />
           </div>
-          <div>
-            <label htmlFor="ingredients">Ingredients:</label>
+          <div className={styles.containTextarea}>
+            <label htmlFor="ingredients">Ingredientes:</label>
             <textarea
               id="ingredients"
               name="ingredients"
               value={product.ingredients}
               onChange={handleChange}
+              className={styles.textarea}
+
             />
           </div>
           {product.composition.map((item, index) => (
             <div key={index}>
-              <h4>Composition - {item.name}</h4>
-              <div>
-                <label>Min:</label>
-                <input
-                  type="number"
-                  name="min"
-                  value={item.min}
-                  onChange={(e) => handleCompositionChange(index, e)}
-                />
-              </div>
-              <div>
-                <label>Max:</label>
-                <input
-                  type="number"
-                  name="max"
-                  value={item.max}
-                  onChange={(e) => handleCompositionChange(index, e)}
-                />
-              </div>
-              {item.value && (
+              <h4>Composición - {item.name}</h4>
+              {item.value !== undefined && item.value !== null ? (
                 <div>
-                  <label>Value:</label>
+                  <label>valor:</label>
                   <input
                     type="number"
                     name="value"
@@ -148,14 +152,36 @@ const CreateProduct = () => {
                     onChange={(e) => handleCompositionChange(index, e)}
                   />
                 </div>
+              ) : (
+                <>
+                  <div>
+                    <label>Min:</label>
+                    <input
+                      type="number"
+                      name="min"
+                      value={item.min}
+                      onChange={(e) => handleCompositionChange(index, e)}
+                    />
+                  </div>
+                  <div>
+                    <label>Max:</label>
+                    <input
+                      type="number"
+                      name="max"
+                      value={item.max}
+                      onChange={(e) => handleCompositionChange(index, e)}
+                    />
+                  </div>
+                </>
               )}
             </div>
           ))}
+
+          <h3>Guia alimentaria:</h3>
           {product.feedingGuide.map((guide, index) => (
             <div key={index}>
-              <h4>Feeding Guide</h4>
               <div>
-                <label>Weight Min:</label>
+                <label>Peso Min:</label>
                 <input
                   type="number"
                   name="peso_min"
@@ -164,7 +190,7 @@ const CreateProduct = () => {
                 />
               </div>
               <div>
-                <label>Weight Max:</label>
+                <label>Peso Max:</label>
                 <input
                   type="number"
                   name="peso_max"
@@ -173,7 +199,7 @@ const CreateProduct = () => {
                 />
               </div>
               <div>
-                <label>Ration Min:</label>
+                <label>Racion Min:</label>
                 <input
                   type="number"
                   name="racion_min"
@@ -182,7 +208,7 @@ const CreateProduct = () => {
                 />
               </div>
               <div>
-                <label>Ration Max:</label>
+                <label>Racion Max:</label>
                 <input
                   type="number"
                   name="racion_max"
@@ -190,10 +216,17 @@ const CreateProduct = () => {
                   onChange={(e) => handleFeedingGuideChange(index, e)}
                 />
               </div>
+              <button type="button" onClick={() => removeFeedingGuide(index)}>
+                Eliminar
+              </button>
             </div>
           ))}
+          <button type="button" onClick={addFeedingGuide}>
+            Añadir Ración
+          </button>
+
           <div>
-            <label htmlFor="images">Product Images:</label>
+            <label htmlFor="images">Imagenes:</label>
             <input
               type="file"
               id="images"
@@ -203,7 +236,7 @@ const CreateProduct = () => {
             />
           </div>
           <div>
-            <label htmlFor="stock">Stock Amount:</label>
+            <label htmlFor="stock">Stock:</label>
             <input
               type="number"
               id="stock"
@@ -218,24 +251,28 @@ const CreateProduct = () => {
             />
           </div>
           <div>
-            <label htmlFor="categories">Categories (comma separated):</label>
+            <label htmlFor="categories">Categoria (separado por comas):</label>
             <input
               type="text"
               id="categories"
               name="categories"
               onChange={handleCategoryChange}
+              placeholder="Medianos,Grandes"
             />
           </div>
           <div>
-            <label htmlFor="types">Types (comma separated):</label>
+            <label htmlFor="types">Tipo o raza (separado por comas):</label>
             <input
               type="text"
               id="types"
               name="types"
               onChange={handleTypeChange}
+              placeholder="Gatos,Perros"
             />
           </div>
-          <button type="submit">Create Product</button>
+          <button type="submit" className={styles.button}>
+            Create Product
+          </button>
         </form>
       </div>
     </div>
