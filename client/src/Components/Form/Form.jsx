f5c71dcf45ccdd84ef6f7d714c4bc423ac73ae9e
valen07dido <valendido69@gmail.com>
2024-09-30 09:52:09 -0300

    ajustando form

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -59,7 +59,7 @@ const Form = () => {
         confirmButtonText: "Entendido",
       });
       swal.showLoading();
-  
+
       const response = await fetch(`${url}/send`, {
         method: "POST",
         headers: {
@@ -67,8 +67,7 @@ const Form = () => {
         },
         body: JSON.stringify(data),
       });
-  
-  
+
       if (response.error) {
         swal.fire({
           title: "Algo falló",
@@ -107,154 +106,160 @@ const Form = () => {
       });
     }
   };
-  
+
   return (
-    <div className={styles.container}>
-      <div>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <h1>Contáctanos</h1>
-          <h3>¡Recibe lista de precios y novedades!</h3>
+    <div className={styles.globalContainer}>
+      <div className={styles.container}>
+        <div>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <h1>Contáctanos</h1>
+            <h3>¡Recibe lista de precios y novedades!</h3>
 
-          {/* Nombre */}
-          <div className={styles.flex1}>
-            <div
-              className={errors.e1 ? styles.inputGroupError : styles.inputGroup}
-            >
-              <input
-                type="text"
-                name="name"
-                id="name"
-                onChange={handleChange}
-                value={data.name}
-                required
-              />
-              <label htmlFor="name">Nombre</label>
-              {errors.e1 && <p className={styles.error}>*{errors.e1}*</p>}
-            </div>
-
-            {/* Teléfono */}
-            <div
-              className={errors.e3 ? styles.inputGroupError : styles.inputGroup}
-            >
-              <input
-                type="tel"
-                name="phone"
-                id="phone"
-                onChange={handleChange}
-                value={data.phone}
-                required
-              />
-              <label htmlFor="phone">Teléfono</label>
-              {errors.e3 && <p className={styles.error}>*{errors.e3}*</p>}
-            </div>
-          </div>
-
-          {/* Email */}
-          <div
-            className={errors.e2 ? styles.inputGroupError : styles.inputGroup}
-          >
-            <input
-              type="email"
-              name="email"
-              id="email"
-              onChange={handleChange}
-              value={data.email}
-              required
-            />
-            <label htmlFor="email">Email</label>
-            {errors.e2 && <p className={styles.error}>*{errors.e2}*</p>}
-          </div>
-
-          {/* Provincia y Localidad */}
-          <div className={styles.flex1}>
-            <div className={styles.divSelect}>
-              <label htmlFor="province">Provincia</label>
-              <select
-                name="province"
-                id="province"
-                value={data.province}
-                onChange={handleProvinceChange}
-                required
-                className={styles.select}
+            {/* Nombre */}
+            <div className={styles.flex1}>
+              <div
+                className={
+                  errors.e1 ? styles.inputGroupError : styles.inputGroup
+                }
               >
-                <option value="">Seleccione una provincia</option>
-                {Array.from(new Set(localities.map((loc) => loc.province))).map(
-                  (province, index) => (
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={handleChange}
+                  value={data.name}
+                  required
+                />
+                <label htmlFor="name">Nombre</label>
+                {errors.e1 && <p className={styles.error}>*{errors.e1}*</p>}
+              </div>
+
+              {/* Teléfono */}
+              <div
+                className={
+                  errors.e3 ? styles.inputGroupError : styles.inputGroup
+                }
+              >
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  onChange={handleChange}
+                  value={data.phone}
+                  required
+                />
+                <label htmlFor="phone">Teléfono</label>
+                {errors.e3 && <p className={styles.error}>*{errors.e3}*</p>}
+              </div>
+            </div>
+
+            {/* Email */}
+            <div
+              className={errors.e2 ? styles.inputGroupError : styles.inputGroup}
+            >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleChange}
+                value={data.email}
+                required
+              />
+              <label htmlFor="email">Email</label>
+              {errors.e2 && <p className={styles.error}>*{errors.e2}*</p>}
+            </div>
+
+            {/* Provincia y Localidad */}
+            <div className={styles.flex1}>
+              <div className={styles.divSelect}>
+                <label htmlFor="province">Provincia</label>
+                <select
+                  name="province"
+                  id="province"
+                  value={data.province}
+                  onChange={handleProvinceChange}
+                  required
+                  className={styles.select}
+                >
+                  <option value="">Seleccione una provincia</option>
+                  {Array.from(
+                    new Set(localities.map((loc) => loc.province))
+                  ).map((province, index) => (
                     <option key={index} value={province}>
                       {province}
                     </option>
-                  )
-                )}
-              </select>
+                  ))}
+                </select>
+              </div>
+
+              <div className={styles.divSelect}>
+                <label htmlFor="locality">Localidad</label>
+                <select
+                  name="locality"
+                  id="locality"
+                  value={data.locality}
+                  onChange={handleLocalityChange}
+                  disabled={!data.province}
+                  required
+                  className={styles.select}
+                >
+                  <option value="">Seleccione una localidad</option>
+                  {filteredLocalities.map((loc, index) => (
+                    <option key={index} value={loc.name}>
+                      {loc.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
 
+            {/* Tipo de cliente */}
             <div className={styles.divSelect}>
-              <label htmlFor="locality">Localidad</label>
+              <label htmlFor="Type">Tipo de cliente</label>
               <select
-                name="locality"
-                id="locality"
-                value={data.locality}
-                onChange={handleLocalityChange}
-                disabled={!data.province}
+                name="Type"
+                id="Type"
+                value={data.Type}
+                onChange={handleChange}
                 required
                 className={styles.select}
               >
-                <option value="">Seleccione una localidad</option>
-                {filteredLocalities.map((loc, index) => (
-                  <option key={index} value={loc.name}>
-                    {loc.name}
-                  </option>
-                ))}
+                <option value="">Seleccione su negocio</option>
+                <option value="soy particular">Soy particular</option>
+                <option value="tengo un pet shop">Tengo un pet shop</option>
+                <option value="vendo online">Vendo online</option>
+                <option value="estoy comenzando un emprendimiento">
+                  Estoy comenzando un emprendimiento
+                </option>
+                <option value="tengo un refugio">Tengo un refugio</option>
+                <option value="soy criador">Soy criador</option>
+                <option value="otro">Otro</option>
               </select>
             </div>
-          </div>
 
-          {/* Tipo de cliente */}
-          <div className={styles.divSelect}>
-            <label htmlFor="Type">Tipo de cliente</label>
-            <select
-              name="Type"
-              id="Type"
-              value={data.Type}
-              onChange={handleChange}
-              required
-              className={styles.select}
+            {/* Mensaje */}
+            <div
+              className={errors.e4 ? styles.inputGroupError : styles.inputGroup}
             >
-              <option value="">Seleccione su negocio</option>
-              <option value="soy particular">Soy particular</option>
-              <option value="tengo un pet shop">Tengo un pet shop</option>
-              <option value="vendo online">Vendo online</option>
-              <option value="estoy comenzando un emprendimiento">
-                Estoy comenzando un emprendimiento
-              </option>
-              <option value="tengo un refugio">Tengo un refugio</option>
-              <option value="soy criador">Soy criador</option>
-              <option value="otro">Otro</option>
-            </select>
-          </div>
+              <textarea
+                name="message"
+                placeholder="Ingrese su mensaje aquí..."
+                cols="100%"
+                rows="10"
+                required
+                onChange={handleChange}
+                value={data.message}
+                className={errors.e4 ? styles.messageError : styles.message}
+              ></textarea>
+              {errors.e4 && <p className={styles.error}>*{errors.e4}*</p>}
+            </div>
 
-          {/* Mensaje */}
-          <div
-            className={errors.e4 ? styles.inputGroupError : styles.inputGroup}
-          >
-            <textarea
-              name="message"
-              placeholder="Ingrese su mensaje aquí..."
-              cols="100%"
-              rows="10"
-              required
-              onChange={handleChange}
-              value={data.message}
-              className={errors.e4 ? styles.messageError : styles.message}
-            ></textarea>
-            {errors.e4 && <p className={styles.error}>*{errors.e4}*</p>}
-          </div>
-
-          {/* Botón de envío */}
-          <button className={styles.buttons} type="submit">
-            Enviar mensaje
-          </button>
-        </form>
+            {/* Botón de envío */}
+            <button className={styles.buttons} type="submit">
+              Enviar mensaje
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
